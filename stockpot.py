0f65b2d6fe596f5fc0415afdc6f6de5d569aa925
Jeff Lee <jlee@fool.com>
2014-02-28 11:26:30 -0500

    really working

--- stockpot.py
+++ stockpot.py
@@ -1,13 +1,13 @@
-from flask import Flask, send_from_directory, send_static_file
+from flask import Flask, send_from_directory
 import requests
 
 
 app = Flask(__name__)
 
 
-@app.route("/universe")
+@app.route("/")
 def index():
-    return send_static_file('index.html')
+    return app.send_static_file('index.html')
 
 
 @app.route("/<path:filename>")
