4511d8c5b2a63c41277279df17098aed48cee41e
Jeff Lee <jlee@fool.com>
2014-02-28 15:43:48 -0500

    loop

--- stockpot.py
+++ stockpot.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
 import requests
 import json
 
 
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST', ])
 def index():
     r = requests.get("https://api.fool.com/premium/recommendations/tmfuniverse/?apikey=J6tv7JMuX6DRTqgWbhFIR8pKEww4YV81")
     stocks = json.loads(r.content)
-    print stocks
     return render_template('index.html', stocks=stocks)
+
+
+@app.route("/soup", methods=['GET', 'POST', ])
+def soup():
+    picks = request.form.getlist('picks')
+    return render_template('soup.html', picks=picks)
+
+
+@app.route("/static/<path:filename>")
+def show_static(filename):
+    return send_from_directory('static', filename)
 
 
 @app.route("/universe")
