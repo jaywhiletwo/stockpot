2fcae694478b86a141ba5dd9025527448fd6cc7e
Jeff Lee <jlee@fool.com>
2014-02-28 10:40:56 -0500

    add backbone

--- stockpot.py
+++ stockpot.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import requests
+import json
 
 
 app = Flask(__name__)
@@ -8,7 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     r = requests.get("https://api.fool.com/premium/recommendations/tmfuniverse/?apikey=J6tv7JMuX6DRTqgWbhFIR8pKEww4YV81")
-    return r.content
+    universe = json.loads(r.content)
+    return str(universe)
+    
 
 if __name__ == "__main__":
         app.run()
