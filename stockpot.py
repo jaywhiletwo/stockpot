cb676716dbe4e6adf8fe142ad623dbf5d78c31bb
Jeff Lee <jlee@fool.com>
2014-02-28 10:54:30 -0500

    universe and index

--- stockpot.py
+++ stockpot.py
@@ -6,12 +6,16 @@ import json
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/universe")
+def api():
     r = requests.get("https://api.fool.com/premium/recommendations/tmfuniverse/?apikey=J6tv7JMuX6DRTqgWbhFIR8pKEww4YV81")
     universe = json.loads(r.content)
     return str(universe)
-    
+
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
