592c6eb41b265cd259b3b412c48825164aa30541
Jeff Lee <jlee@fool.com>
2014-02-28 10:17:15 -0500

    api

--- stockpot.py
+++ stockpot.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import requests
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    r = requests.get("https://api.fool.com/premium/recommendations/tmfuniverse/?apikey=J6tv7JMuX6DRTqgWbhFIR8pKEww4YV81")
+    return r.content
 
 if __name__ == "__main__":
         app.run()
