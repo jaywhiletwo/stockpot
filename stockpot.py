7c5999a3383c5301038fd4a09808d012f2ab0bbc
Jeff Lee <jlee@fool.com>
2014-02-28 16:19:22 -0500

    your soup

--- stockpot.py
+++ stockpot.py
@@ -15,8 +15,9 @@ def index():
 
 @app.route("/soup", methods=['GET', 'POST', ])
 def soup():
-    picks = request.form.getlist('picks')
-    return render_template('soup.html', picks=picks)
+    picks = ['AAPL', 'MSFT', ]
+    your_soup = 'Rule Breakers'
+    return render_template('soup.html', picks=picks, your_soup=your_soup)
 
 
 @app.route("/static/<path:filename>")
@@ -26,14 +27,15 @@ def show_static(filename):
 
 @app.route("/universe")
 def api():
-    return collapsed_universe()
+    return str(collapsed_universe())
 
 
 def collapsed_universe():
     r = requests.get("https://api.fool.com/premium/recommendations/tmfuniverse/?apikey=J6tv7JMuX6DRTqgWbhFIR8pKEww4YV81")
     uni = []
-    for ticker in json.loads(r.content):
+    for ticker in sorted(json.loads(r.content), key=lambda x: x['OpenDate']):
         uni.append(ticker)
+
     return uni
 
 
